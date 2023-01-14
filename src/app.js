22fce0dcb619e4f03da27a0f973d1bce8399542a
Nanda-Souza <fesouoli@gmail.com>
2023-01-14 18:19:02 -0300

    participant POST method using JOI validation

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors"
 import { MongoClient, ObjectId } from "mongodb";
 import dotenv from 'dotenv'
+import joi from 'joi'
 dotenv.config();
 
 
@@ -21,6 +22,30 @@ const server = express()
 
 server.use(cors())
 server.use(express.json());
+
+
+
+server.post('/participants', async (req, res) => {
+    const { name } = req.body;
+
+    //JOI validation schema so {name} variable can be required and of String type
+    const participantSchema = joi.object({
+        name: joi.string().required()
+    })
+
+    //Execute the JOI validation, if it fails will return an error on validateParticipant
+    const validateParticipant = participantSchema.validate({name}, { abortEarly: true })
+    console.log(validateParticipant)
+    //If theres and error 422 error is early returned
+    if (validateParticipant.error) {
+        return res.sendStatus(422)
+      }
+
+    
+    return res.status(200).send("Post Validation is working!")
+    
+  });
+
 
 const PORT = 5000
 
